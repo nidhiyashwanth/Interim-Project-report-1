0521329b57395f61c6f72fca5f9b2b39a8e70ef6
nidhiyashwanth <nidhiyashwanth007@gmail.com>
2022-09-11 18:13:48 +0530

    first commit

---
+++ local.js
@@ -0,0 +1,36 @@
+let colleges = [
+    { 
+        "College_Name" : "GEC Gangavathi", 
+        "Annual_Fee" : "25000",
+        "Caste" : "GEN", 
+        "Course" : ["BSC", "BTECH", "BE"], 
+        "Scholarship_Amount" : "12500",       
+        "Scholarship_Marks" : "90.00"
+    },
+    { 
+        "College_Name" : "Vasavi College, Bangalore", 
+        "Annual_Fee" : "190000",
+        "Caste" : ["GEN", "OBC"],
+        "Course" : ["BSC", "BTECH", "BE","MTECH"], 
+        "Scholarship_Amount" : ["30500","52000"],
+        "Scholarship_Marks" : ["93.00", "80.00"]
+    },
+    { 
+        "College_Name" : "IIT Bombay", 
+        "Annual_Fee" : "200000",
+        "Caste" : ["GEN", "OBC", "ST", "SC"],
+        "Course" : ["BSC", "BTECH", "BE", "MTECH", "MSC", "MS"], 
+        "Scholarship_Amount" : ["35000", "52500", "76000", "82000"],       
+        "Scholarship_Marks" : ["96.00", "93.00", "91.00", "90.00"]
+    },
+    {
+        "College_Name" : "GEC Raichur", 
+        "Annual_Fee" : "45000",
+        "Caste" : ["GEN", "OBC", "SC"],
+        "Course" : ["BSC", "BTECH", "BE", "MTECH", "MS"], 
+        "Scholarship_Amount" : ["3570", "8250", "12500"],       
+        "Scholarship_Marks" : ["86.00", "80.00", "70.00"]
+    }
+]
+
+console.log(colleges)
