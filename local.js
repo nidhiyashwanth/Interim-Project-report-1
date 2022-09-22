1841eca015643b63222dfac6b2ec787f53353fd6
Nidhi Yashwanth <65482596+nidhiyashwanth@users.noreply.github.com>
2022-09-22 19:46:04 +0530

    made some changes

--- local.js
+++ local.js
@@ -1,31 +1,249 @@
-let colleges = [{
-	"College_Name": "GEC Gangavathi",
-	"Annual_Fee": "25000",
-	"Caste": "GEN",
-	"Course": ["BSC", "BTECH", "BE"],
-	"Scholarship_Amount": "12500",
-	"Scholarship_Marks": "90.00"
-}, {
-	"College_Name": "Vasavi College, Bangalore",
-	"Annual_Fee": "190000",
-	"Caste": ["GEN", "OBC"],
-	"Course": ["BSC", "BTECH", "BE", "MTECH"],
-	"Scholarship_Amount": ["30500", "52000"],
-	"Scholarship_Marks": ["93.00", "80.00"]
-}, {
-	"College_Name": "IIT Bombay",
-	"Annual_Fee": "200000",
-	"Caste": ["GEN", "OBC", "ST", "SC"],
-	"Course": ["BSC", "BTECH", "BE", "MTECH", "MSC", "MS"],
-	"Scholarship_Amount": ["35000", "52500", "76000", "82000"],
-	"Scholarship_Marks": ["96.00", "93.00", "91.00", "90.00"]
-}, {
-	"College_Name": "GEC Raichur",
-	"Annual_Fee": "45000",
-	"Caste": ["GEN", "OBC", "SC"],
-	"Course": ["BSC", "BTECH", "BE", "MTECH", "MS"],
-	"Scholarship_Amount": ["3570", "8250", "12500"],
-	"Scholarship_Marks": ["86.00", "80.00", "70.00"]
-}];
+//College JSON data
+const colleges = [
+    {
+        "College_Name" : "GEC Gangavathi",
+        "Annual_Fee" : "25000",
+        "Caste" : "General",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "12000",
+        "Scholarship_Marks" : "90"
+    },
+    {
+        "College_Name" : "GEC Gangavathi",
+        "Annual_Fee" : "20000",
+        "Caste" : "Scheduled Caste (SC)",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "15000",
+        "Scholarship_Marks" : "85"
+    },
+    {
+        "College_Name" : "GEC Gangavathi",
+        "Annual_Fee" : "18000",
+        "Caste" : "Scheduled Tribes (ST)",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "18000",
+        "Scholarship_Marks" : "85"
+    },
+    {
+        "College_Name" : "GEC Gangavathi",
+        "Annual_Fee" : "35000",
+        "Caste" : "General",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "20000",
+        "Scholarship_Marks" : "90"
+    },
+    {
+        "College_Name" : "GEC Gangavathi",
+        "Annual_Fee" : "25000",
+        "Caste" : "Scheduled Caste (SC)",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "25000",
+        "Scholarship_Marks" : "85"
+    },
+    {
+        "College_Name" : "GEC Gangavathi",
+        "Annual_Fee" : "25000",
+        "Caste" : "Scheduled Tribes (ST)",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "30000",
+        "Scholarship_Marks" : "85"
+    },
+    {
+        "College_Name" : "Vasavi College, Bangalore",
+        "Annual_Fee" : "80000",
+        "Caste" : "General",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "30000",
+        "Scholarship_Marks" : "80"
+    },
+    {
+        "College_Name" : "Vasavi College, Bangalore",
+        "Annual_Fee" : "80000",
+        "Caste" : "Scheduled Caste (SC)",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "50000",
+        "Scholarship_Marks" : "60"
+    },
+    {
+        "College_Name" : "Vasavi College, Bangalore",
+        "Annual_Fee" : "80000",
+        "Caste" : "Scheduled Tribes (ST)",
+        "Course" : "BSC",
+        "Scholarship_Amount" : "40000",
+        "Scholarship_Marks" : "50"
+    },
+    {
+        "College_Name" : "Vasavi College, Bangalore",
+        "Annual_Fee" : "190000",
+        "Caste" : "General",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "90000",
+        "Scholarship_Marks" : "90"
+    },
+    {
+        "College_Name" : "Vasavi College, Bangalore",
+        "Annual_Fee" : "150000",
+        "Caste" : "Scheduled Caste (SC)",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "100000",
+        "Scholarship_Marks" : "70"
+    },
+    {
+        "College_Name" : "Vasavi College, Bangalore",
+        "Annual_Fee" : "150000",
+        "Caste" : "Scheduled Tribes (ST)",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "120000",
+        "Scholarship_Marks" : "60"
+    },
 
-console.log(colleges);
+	{
+        "College_Name" : "Vasavi College, Bangalore",
+        "Annual_Fee" : "225000",
+        "Caste" : "General",
+        "Course" : "MTECH",
+        "Scholarship_Amount" : "100000",
+        "Scholarship_Marks" : "90"
+    },
+	{
+        "College_Name" : "Vasavi College, Bangalore",
+        "Annual_Fee" : "225000",
+        "Caste" : "Scheduled Caste (SC)",
+        "Course" : "MTECH",
+        "Scholarship_Amount" : "125000",
+        "Scholarship_Marks" : "75"
+    },
+	{
+        "College_Name" : "Vasavi College, Bangalore",
+        "Annual_Fee" : "225000",
+        "Caste" : "Scheduled Tribes (ST)",
+        "Course" : "MTECH",
+        "Scholarship_Amount" : "130000",
+        "Scholarship_Marks" : "60"
+    },
+
+    {
+        "College_Name" : "IIT Bombay",
+        "Annual_Fee" : "40000",
+        "Caste" : "General",
+		"Course" : "BSC",
+		"Scholarship_Amount" : "25000",
+		"Scholarship_Marks" : "95"
+    },
+
+    {
+        "College_Name" : "IIT Bombay",
+        "Annual_Fee" : "40000",
+		"Caste" : "Scheduled Caste (SC)",
+		"Course" : "BSC",
+		"Scholarship_Amount" : "30000",
+		"Scholarship_Marks" : "90"
+    },
+    {
+        "College_Name" : "IIT Bombay",
+        "Annual_Fee" : "40000",
+        "Caste" : "Scheduled Tribes (ST)",
+		"Course" : "BSC",
+		"Scholarship_Amount" : "40000",
+		"Scholarship_Marks" : "85"
+    },
+    {
+        "College_Name" : "IIT Bombay",
+        "Annual_Fee" : "115000",
+        "Caste" : "General",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "60000",
+        "Scholarship_Marks" : "95"
+    },
+    {
+        "College_Name" : "IIT Bombay",
+        "Annual_Fee" : "115000",
+        "Caste" : "Scheduled Caste (SC)",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "95000",
+        "Scholarship_Marks" : "90"
+    },
+    {
+        "College_Name" : "IIT Bombay",
+        "Annual_Fee" : "115000",
+        "Caste" : "Scheduled Tribes (ST)",
+        "Course" : "BTECH",
+        "Scholarship_Amount" : "115000",
+        "Scholarship_Marks" : "90"
+    },
+	{
+		"College_Name": "GEC Raichur",
+		"Annual_Fee": "30000",
+		"Caste": "General",
+		"Course": "BSC",
+		"Scholarship_Amount": "8250",
+		"Scholarship_Marks": "85",
+	},
+
+	{
+		"College_Name": "GEC Raichur",
+		"Annual_Fee": "30000",
+		"Caste": "Scheduled Caste (SC)",
+		"Course": "BSC",
+		"Scholarship_Amount": "12500",
+		"Scholarship_Marks": "80"
+	},
+
+	{
+		"College_Name": "GEC Raichur",
+		"Annual_Fee": "30000",
+		"Caste": "Scheduled Tribes (ST)",
+		"Course": "BSC",
+		"Scholarship_Amount": "15000",
+		"Scholarship_Marks": "70",
+	},
+
+	{
+		"College_Name": "GEC Raichur",
+		"Annual_Fee": "45000",
+		"Caste": "General",
+		"Course": "BTECH",
+		"Scholarship_Amount": "8500",
+		"Scholarship_Marks": "90",
+	},
+	{
+		"College_Name": "GEC Raichur",
+		"Annual_Fee": "45000",
+		"Caste": "Scheduled Caste (SC)",
+		"Course": "BTECH",
+		"Scholarship_Amount": "12000",
+		"Scholarship_Marks": "85",
+	},
+	{
+		"College_Name": "GEC Raichur",
+		"Annual_Fee": "45000",
+		"Caste": "Scheduled Tribes (ST)",
+		"Course": "BTECH",
+		"Scholarship_Amount": "18500",
+		"Scholarship_Marks": "60",
+	},
+	{
+		"College_Name": "GEC Raichur",
+		"Annual_Fee": "60000",
+		"Caste": "General",
+		"Course": "MTECH",
+		"Scholarship_Amount": "15000",
+		"Scholarship_Marks": "85"
+	},
+	{
+		"College_Name": "GEC Raichur",
+		"Annual_Fee": "60000",
+		"Caste": "Scheduled Caste (SC)",
+		"Course": "MTECH",
+		"Scholarship_Amount": "30000",
+		"Scholarship_Marks": "60"
+	},
+	{
+		"College_Name": "GEC Raichur",
+		"Annual_Fee": "45000",
+		"Caste": "Scheduled Tribes (ST)",
+		"Course": "MTECH",
+		"Scholarship_Amount": "45000",
+		"Scholarship_Marks": "50"
+	}
+];
